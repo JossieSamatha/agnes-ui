cbd1a3eaba386347b0bceff2558791339496cf24
程泽群 <zq.cheng@goupwith.com>
2020-07-17 15:59:42 +0800

    AGNES-36 工作日配置 [http://jira.goupwith.com/browse/AGNES-36]

--- src/modules/agnes-ac/pages/config/workday/index.vue
+++ src/modules/agnes-ac/pages/config/workday/index.vue
@@ -2,9 +2,8 @@
     <div>
         <el-row>
             <el-col  :span="4" style="height:100%">
-                <div  style="height:100%;border:1px solid #000">
+                <div  style="height:100%;border:1px solid #F6F8FA;border-radius:5px">
                     <el-button size="small">同步</el-button>
-
                     <div v-for="(item,index) in areaList" :key="index"
                         :v-model="queryParam.workdayAreaCode"
                     @click="choseOptions(index,item)"
@@ -15,14 +14,39 @@
             </el-col>
             <el-col :span="20">
                 <div>
-                    <gf-grid  @row-double-click="editWorkday"
-                    :query-args="queryParam"
-                    @grid-ready="onReady"
-                    grid-no="agnes-workday" ref="grid">
-                    <template slot="right-before">
-                        <gf-button  @click="addWorkday" size="mini">添加</gf-button>
-                    </template>
-                    </gf-grid>
+                    <el-calendar>
+                        <template
+                            slot="dateCell"
+                            slot-scope="{date, data}" >
+                            <div v-for="item in calendarData"  v-bind:key="item.day">
+                                <div v-if="data.day.toString()==item.bizDate">
+                                    <div slot="reference"   
+                                     :class="item.workday==='0'?'txt-color':''">
+                                        {{ data.day.split('-').slice(2,3).toString() }}
+                                    </div>
+                                    <el-popover
+                                        placement="top-start"
+                                        width="200"
+                                        trigger="hover">
+                                        <div >
+                                            <el-row>
+                                                <el-col>
+                                                    <el-button @click="onUpdateWorkday(item,'0')">设置为节假日</el-button>
+                                                </el-col>
+                                            </el-row>
+                                            <el-row>
+                                                <el-col>
+                                                    <el-button @click="onUpdateWorkday(item,'1')">设置为工作日</el-button>
+                                                </el-col>
+                                            </el-row>
+                                        </div>
+                                        <div style="height:300px;width:100%"  slot="reference">
+                                        </div>
+                                    </el-popover>
+                                </div>
+                            </div>
+                        </template>
+                    </el-calendar>
                 </div>
             </el-col>
         </el-row>
@@ -30,97 +54,77 @@
 </template>
 
 <script>
-import WorkdayDlg from "./workday-dlg.vue";
-
 export default {
-
-    data(){
-        return{
-            flag: 0,
+    data() {
+        return {
+            workday:{
+                date:"",
+                workdayAreaCode:"01",
+                workday:"",
+            },
+            calendarData: {
+                data:[]
+            },
             areaList:[],
             queryParam:{
                 workdayAreaCode :"01"
-            }
+            },
+            flag:0
         }
     },
-
-    mounted() {
+    mounted(){
+        this.onListWorkday(this.workday);
         let dictTypeId="AC_AREA";
         this.areaList=this.$app.dict.getDictItems(dictTypeId);
     },
+    watch:{
+        calendarData(val){
+            this.calendarData=val;
+        },
+         
+    },
     methods:{
-        onReady() {
-            this.grid = this.$refs.grid;
-            this.refreshRows();
+        list(workday){
+            this.onListWorkday(workday);
+        },
+        async onListWorkday(workday){
+            try {
+                const resp =await this.$api.workdayConfigApi.getWorkdayList(workday);
+                this.calendarData=resp.data;
+            } catch (reason) {
+                this.$msg.error(reason);
+            }
+        },
+        async onUpdateWorkday(item,flag){
+            this.workday.workdayId=item.workdayId;
+            this.workday.bizDate=item.bizDate;
+            this.workday.workdayAreaCode=item.workdayAreaCode;
+            this.workday.workday=flag;
+
+            try {
+                await this.$api.workdayConfigApi.updateWorkday(this.workday);
+                this.list(this.workday)
+                this.$msg.success('保存成功');
+            } catch (reason) {
+                this.$msg.error(reason);
+            }
         },
         choseOptions(id,item){
             this.flag=id;
             this.queryParam.workdayAreaCode=item.dictId;
-            this.list(this.queryParam);
+            this.onListWorkday(this.queryParam);
         },
-        async list(params){
-            const resp = await this.$api.workdayConfigApi.getWorkdayList(params);
-            this.grid.setRowData(resp.data);
-
-        },
-        refreshRows() {
-            this.grid.setRowData(this.rowData);
-        },
-        reloadData() {
-            this.$refs.grid.reloadData();
-        },
-        showDlg(mode, row, actionOk) {
-            if (mode !== 'add' && !row) {
-                this.$msg.warning("请选中一条记录!");
-                return;
-            }
-            this.$nav.showDialog(
-                WorkdayDlg,
-                {
-                    args: {row, mode, actionOk},
-                    width: '50%',
-                    title: this.$dialog.formatTitle('工作日', mode),
-                }
-            );
-        },
-        async onAddWorkday() {
-            this.reloadData();
-        },
-        addWorkday() {
-            this.showDlg('add', {}, this.onAddWorkday.bind(this));
-        },
-        editWorkday(params) {
-            this.showDlg('edit', params.data, this.onEditWorkday.bind(this));
-        },
-        async onEditWorkday(workday, row) {
-            Object.assign(row, workday);
-            // this.refreshRows();
-        },
-        async deleteWorkday(params) {
-            const row = params.data;
-            const ok = await this.$msg.ask(`确认删除字段:[${row.bizDate}]吗, 是否继续?`);
-            if (!ok) {
-                return
-            }
-            try {
-                const p = this.$api.workdayConfigApi.deleteWorkday(row.workdayId);
-                await this.$app.blockingApp(p);
-                this.refreshRows();
-            } catch (reason) {
-                this.$msg.error(reason);
-            }
-        }
-
-
     }
 }
+
+
 </script>
 
 <style >
-
+.txt-color{
+    color: red;
+}
 .act-button{
     background-color:#F6F8FA;
 }
-
-
 </style>
