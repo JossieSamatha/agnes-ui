07bd1e037e78514f1f1bb505f62d6a336b8543c1
matian <tian.ma@goupwith.com>
2020-07-22 22:37:18 +0800

    AGNES-44 解析CASE配置json，以及优化 http://jira.goupwith.com/browse/AGNES-44

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
-                    '^/api/agnes-dop': '/'
+                    '^/api/agnes-ac': '/'
                 }
             },
             '/api/agnes-app': {
