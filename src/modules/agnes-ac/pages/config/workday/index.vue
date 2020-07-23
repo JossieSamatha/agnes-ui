c1c66db0eb915c99eeebbce70fc799f4a87bfdb7
程泽群 <zq.cheng@goupwith.com>
2020-07-23 14:37:53 +0800

    AGNES-36 工作日表 [http://jira.goupwith.com/browse/AGNES-36]

--- src/modules/agnes-ac/pages/config/workday/index.vue
+++ src/modules/agnes-ac/pages/config/workday/index.vue
@@ -1,33 +1,33 @@
 <template>
     <div>
         <el-row>
-            <el-col  :span="4" style="height:100%">
+            <el-col  :span="4" >
                 <div  style="height:100%;border:1px solid #F6F8FA;border-radius:5px">
                     <el-button size="small">同步</el-button>
                     <div v-for="(item,index) in areaList" :key="index"
-                        :v-model="queryParam.workdayAreaCode"
-                    @click="choseOptions(index,item)"
-                    v-bind:class="{'act-button':flag==index}">
+                         :v-model="queryParam.workdayAreaCode"
+                         @click="choseOptions(index,item)"
+                         v-bind:class="{'act-button':flag==index}">
                         <gf-button type="text" >{{item.dictName}}</gf-button>
                     </div>
                 </div>
             </el-col>
             <el-col :span="20">
-                <div>
-                    <el-calendar>
+                <div >
+                    <el-calendar >
                         <template
-                            slot="dateCell"
-                            slot-scope="{date, data}" >
+                                slot="dateCell"
+                                slot-scope="{date, data}" >
                             <div v-for="item in calendarData"  v-bind:key="item.day">
                                 <div v-if="data.day.toString()==item.bizDate">
-                                    <div slot="reference"   
-                                     :class="item.workday==='0'?'txt-color':''">
+                                    <div slot="reference"
+                                         :class="item.workday==='0'?'txt-color':''">
                                         {{ data.day.split('-').slice(2,3).toString() }}
                                     </div>
                                     <el-popover
-                                        placement="top-start"
-                                        width="200"
-                                        trigger="hover">
+                                            placement="top-start"
+                                            width="200"
+                                            trigger="hover">
                                         <div >
                                             <el-row>
                                                 <el-col>
@@ -58,9 +58,7 @@ export default {
     data() {
         return {
             workday:{
-                date:"",
                 workdayAreaCode:"01",
-                workday:"",
             },
             calendarData: {
                 data:[]
@@ -73,15 +71,18 @@ export default {
         }
     },
     mounted(){
-        this.onListWorkday(this.workday);
+
         let dictTypeId="AC_AREA";
         this.areaList=this.$app.dict.getDictItems(dictTypeId);
+        this.workday.workdayAreaCode=this.areaList[0].dictId;
+        this.onListWorkday(this.workday);
+
     },
     watch:{
         calendarData(val){
             this.calendarData=val;
         },
-         
+
     },
     methods:{
         list(workday){
@@ -116,8 +117,6 @@ export default {
         },
     }
 }
-
-
 </script>
 
 <style >
