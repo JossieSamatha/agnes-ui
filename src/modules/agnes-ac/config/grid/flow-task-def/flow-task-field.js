8494e79266529a56292b151647c8efdc07995589
matian <tian.ma@goupwith.com>
2020-07-27 16:41:13 +0800

    AGNES-44 解析CASE配置json，以及优化 http://jira.goupwith.com/browse/AGNES-44

---
+++ src/modules/agnes-ac/config/grid/flow-task-def/flow-task-field.js
@@ -0,0 +1,35 @@
+
+export default {
+    columnDefs: [
+        {headerName: "任务名称", field: "reTaskDef.taskName"},
+        {headerName: "业务场景", field: "reTaskDef.bizType"},
+        {headerName: "业务标签", field: "reTaskDef.bizTag"},
+        {headerName: "业务类型", field: "reTaskDef.taskType"},
+        {headerName: "状态", field: "reTaskDef.taskStatus"},
+        {headerName: "创建时间", field: "reTaskDef.crtTs"},
+        {headerName: "创建人", field: "reTaskDef.crtUser"}
+    ],
+    headerHeight: 40,
+    rowHeight: 37,
+    ext: {
+        fetchUrl: "/agnes-ac/v1/ac/kpi/task/case/list?taskType=1",
+        fetchMethod: 'get',
+        pagingMode: true, //不分页
+        checkboxColumn: 1, //是否显示checkbox列,
+        autoFitColumnMode: 1,
+        enableExportLocal: true,
+        pageOptions: {
+            // 分页大小
+            pageSize: 10,
+            // 可供选择的分页大小（下拉切换分页值）
+            pageSizes: [10, 20, 50, 100],
+            // 显示在状态栏上的页数字的个数
+            pageCount: 5,
+            prevText: "上一页",
+            nextText: "下一页",
+            // 分页工具显示项，例如总页数、当前页、上一页、下一页、等分页功能按钮
+            // 详见ElementUI分页组件
+            layout: "total, sizes, prev, pager, next, jumper"
+        },
+    }
+};
