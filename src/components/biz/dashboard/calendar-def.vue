36fbb63cbee1df2542834ecc011d5fefea3feafc
gy <gy.hu@goupwith.com>
2020-07-27 14:33:17 +0800

    AGNES-53 首页面板调整

---
+++ src/components/biz/dashboard/calendar-def.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-calendar v-model="calendarVal">
+    </el-calendar>
+</template>
+
+<script>
+    export default {
+        props: {
+            calendarVal: {
+                type: String,
+                default: new Date()
+            },
+        }
+    }
+</script>
