27405efd69053972a02edb5a7e238084729c61b2
gy <gy.hu@goupwith.com>
2020-08-04 11:47:34 +0800

    AGNES-189 二阶段任务:相关组件封装 http://jira.goupwith.com/browseAGNES-189 cron组件业务功能项目中封装

--- src/components/common/cron-comp/cron-modal.vue
+++ src/components/common/cron-comp/cron-modal.vue
@@ -1,8 +1,46 @@
 <template>
     <div class="cron-modal">
         <i class="question el-icon-question" title="操作文档"></i>
-        <gf-cron ref="innerVueCron" :cornObj="cron" @change="onSave" @close="onCancel"></gf-cron>
-
+        <gf-cron class="gf-cron-tab"
+                 ref="innerVueCron"
+                 :cornObj="cron"
+                 :showType="showType"
+                 @change="onSave"
+                 @close="onCancel">
+            <el-tab-pane slot="extSettimg">
+                <span slot="label">
+                    <i class="el-icon-setting"></i>
+                    <span class="requireStar" style="position: relative">
+                        <span style="position: absolute;color: #f5222e;left: 0;top: -13px">*</span>
+                        其他配置
+                    </span>
+                </span>
+                <div class="tabBody extSetting">
+                    <el-row>
+                        <span>基准日期</span>
+                        <el-radio-group v-model="baseDate">
+                            <el-radio label="00">系统当前业务日期</el-radio>
+                            <el-radio label="01">系统当前工作日期</el-radio>
+                        </el-radio-group>
+                    </el-row>
+                    <el-row>
+                        <span>日期类型</span>
+                        <el-radio-group v-model="dateType" @change="dateTypeChange">
+                            <el-radio label="00">自然日</el-radio>
+                            <el-radio label="01">工作日</el-radio>
+                        </el-radio-group>
+                    </el-row>
+                    <el-row v-show="dateType == '01'">
+                        <span>节假日顺延</span>
+                        <el-radio-group v-model="holidayDelay">
+                            <el-radio label="00">前一工作日</el-radio>
+                            <el-radio label="01">下一工作日</el-radio>
+                            <el-radio label="02">不顺延</el-radio>
+                        </el-radio-group>
+                    </el-row>
+                </div>
+            </el-tab-pane>
+        </gf-cron>
     </div>
 </template>
 
@@ -10,24 +48,44 @@
     export default {
         name: "GfCronModal",
         props: {
+            showType: {type: String},
             cornObj: {type: String},
             action: Function
         },
         data() {
             return {
-                cron:this.cornObj,
+                cron: this.cornObj,
+                baseDate: '00',
+                dateType: '00',
+                holidayDelay: '02'
+            }
+        },
+        create(){
+            const result = this.cornObj.split("#");
+            if(result.length === 1){
+                this.cron = result[0];
+            }else{
+                this.baseDate = result[0];
+                this.dateType = result[1];
+                this.holidayDelay = result[2];
+                this.cron(result[3]);
             }
         },
         mounted() {
         },
         methods: {
             async onSave (cronValue) {
-                this.cron = cronValue;
-                this.action(this.cron);
+                const result = `${this.baseDate}#${this.dateType}#${this.holidayDelay}#${cronValue}`;
+                this.action(result);
                 this.$dialog.close(this, 'ok');
             },
             onCancel () {
                 this.$dialog.close(this, 'cancel');
+            },
+            dateTypeChange(val){
+                if(val === '00'){
+                    this.holidayDelay = '02';
+                }
             }
         },
     }
