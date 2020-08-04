50ead64c170f55b29113970b92aac937fe7ae0da
王梓 <zi.wang@goupwith.com>
2020-08-04 11:51:20 +0800

    AGNES-128 【参数配置】【联系人维护】——在联系人列表中点击“修改/删除”按钮提示“请选中一条记录”。期望：点击修改/删除打开修改删除窗口 [http://jira.goupwith.com/browse/AGNES-128]

--- src/modules/agnes-ac/pages/config/index.js
+++ src/modules/agnes-ac/pages/config/index.js
@@ -21,7 +21,7 @@ const Loading = {
         Gfui.componentView('agnes.config.model', ModelDef, {title: '对象类型定义'});
         Gfui.componentView('agnes.define.task', TaskDef, {title: '任务定义配置'});
         Gfui.componentView('agnes.config.task', TaskConfig, {title: '任务执行列表'});
-        Gfui.componentView('agnes.define.fun', FunDef, {title: '函数类型定义'});
+        Gfui.componentView('agnes.define.fun', FunDef, {title: '函数定义'});
         Gfui.componentView('agnes.config.workday', WorkDay, {title: '工作日'});
         Gfui.componentView('agnes.config.case', CaseDef, {title: 'case定义'});
         Gfui.componentView('agnes.config.mot', MotDef, {title: 'MOT任务配置'});
--- src/modules/agnes-dop/pages/config/index.js
+++ src/modules/agnes-dop/pages/config/index.js
@@ -8,7 +8,7 @@ const Loading = {
     load: function (Gfui) {
         Gfui.componentView('agnes.org.def', OrgDef, {title: '外部机构维护'});
         Gfui.componentView('agnes.org.type', OrgType, {title: '机构类型维护'});
-        Gfui.componentView('agnes.dop.linkman', Linkman, {title: '联系人'});
+        Gfui.componentView('agnes.dop.linkman', Linkman, {title: '联系人维护'});
         Gfui.componentView('agnes.dop.memo', memoDef, {title: '日历维护'});
 
     }
