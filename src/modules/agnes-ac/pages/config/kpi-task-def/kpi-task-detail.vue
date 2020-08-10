fe6164944a35936babc172d4e6a34d986599fc81
LH <17835418239@163.com>
2020-08-10 11:10:45 +0800

    AGNES-203 【任务配置】任务发布调整 [http://jira.goupwith.com/browse/AGNES-203]

--- src/modules/agnes-ac/pages/config/kpi-task-def/kpi-task-detail.vue
+++ src/modules/agnes-ac/pages/config/kpi-task-def/kpi-task-detail.vue
@@ -268,7 +268,7 @@
 
 <script>
     import loadsh from 'lodash';
-    import ExecTimeEdit from "./exec-time";
+    import ExecTimeEdit from "../../../../../components/biz/exec-time";
     import staticData from '../../../util/dataFormat'
     import initData from '../../../util/initData'
     import UserSelect from '../../../components/biz/kpi-user-select'
