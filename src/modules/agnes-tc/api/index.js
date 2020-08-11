ee76ad68973c6ddee78c448b137e556002fbd8f2
LH <17835418239@163.com>
2020-08-11 11:31:03 +0800

    AGNES-203 任务管理 [http://jira.goupwith.com/browse/AGNES-203]

--- src/modules/agnes-tc/api/index.js
+++ src/modules/agnes-tc/api/index.js
@@ -1,9 +1,11 @@
 import kpiDefineApi from './kpi-def-api';
 import ServiceConfApi from './service-conf-api';
 import taskTodoApi from './task-todo-api';
+import taskManageApi from './task-manage-api';
 
 export default {
     kpiDefineApi,
     taskTodoApi,
     ServiceConfApi,
+    taskManageApi,
 };
--- src/modules/agnes-tc/config/grid/task-manage/task-manage.js
+++ src/modules/agnes-tc/config/grid/task-manage/task-manage.js
@@ -1,37 +1,34 @@
 import column from "../../../../../config/column";
 
 const colButtons = [
-    {key: 'confirmTask', title: '复核'},
-    {key: 'viewTask', title: '作废'},
+    {key: 'checkTask', title: '复核'},
+    {key: 'cancelTask', title: '作废'},
 ];
 
 export default {
     columnDefs: [
-        {headerName: "任务编号", field: "taskName"},
-        {headerName: "任务名称", field: "taskStartTm"},
-        {headerName: "任务发起日期", field: "participants"},
-        // {headerName: "计划完成时间", field: "participants"},
-        {headerName: "实际完成时间", field: "participants"},
-        {headerName: "业务标签", field: "participants",dictType: 'AGNES_BIZ_TAG',
+        {headerName: "任务名称", field: "taskName"},
+        {headerName: "任务类型", field: "taskType",formatType: 'dict', dictType: 'AGNES_TASK_TYPE'},
+        {headerName: "任务编号", field: "taskCode"},
+        {headerName: "任务发起日期", field: "exeTime"},
+        {headerName: "实际完成时间", field: "updateTs",
             valueFormatter: function (params) {
-                if(params.value){
-                    let Ids = params.value.split(',');
-                    return Ids.map((dictId) => {
-                        return  window.$gfui.$app.dict.getDictItem('AGNES_BIZ_TAG',dictId).dictName;
-                    }).join(',');
+                console.log(params);
+                let taskStatus = params.data.taskStatus;
+                if(taskStatus.match(/04|05/)){
+                    return params.value;
                 }
                 return "";
             }},
-        {headerName: "业务类型", field: "participants",formatType: 'dict', dictType: 'AGNES_TASK_TYPE'},
-        {headerName: "状态", field: "participants", dictType: "CASE_TASK_STATUS"},
-        column.buildOpCol(120, colButtons)
+        {headerName: "完成状态", field: "taskStatus", dictType: "AGNES_TASK_MGR_STATUS"},
+        column.buildOpCol(100, colButtons)
     ],
     headerHeight: 40,
     rowHeight: 37,
     ext: {
-        fetchUrl: "/agnes-app/v1/task/todo/list",    //后台查询数据的URL地址
-        fetchMethod: 'get',
-        pagingMode: true, //不分页
+        fetchUrl: "/agnes-app/v1/task/manage/list",    //后台查询数据的URL地址
+        fetchMethod: 'post',
+        pagingMode: true, //分页
         checkboxColumn: 0, //是否显示checkbox列,
         enableExportLocal: true,
         pageOptions: {
@@ -40,7 +37,7 @@ export default {
             // 可供选择的分页大小（下拉切换分页值）
             pageSizes: [10, 20, 50, 100],
             // 显示在状态栏上的页数字的个数
-            pageCount: 5,
+            pageCount: 0,
             prevText: "上一页",
             nextText: "下一页",
             // 分页工具显示项，例如总页数、当前页、上一页、下一页、等分页功能按钮
