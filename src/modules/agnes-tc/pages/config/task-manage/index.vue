4799dd73c1ca440e948478a82a1f913edac29ebe
LH <17835418239@163.com>
2020-08-13 19:39:49 +0800

    AGNES-203 任务配置补充、任务管理补充 [http://jira.goupwith.com/browse/AGNES-203]

--- src/modules/agnes-tc/pages/config/task-manage/index.vue
+++ src/modules/agnes-tc/pages/config/task-manage/index.vue
@@ -60,7 +60,6 @@
                 };
                 this.reloadData();
             },
-
             async getExeTime() {
                 try {
                     const resp = this.$api.changeDataApi.getChangeData();
