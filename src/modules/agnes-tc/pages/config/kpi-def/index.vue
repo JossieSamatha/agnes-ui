a62fd07552f2e0d3bece12b996cd738f9fc7a2be
马天 <tian.ma@goupwith.com>
2020-08-14 16:26:29 +0800

    AGNES-221 待办，已办优化 [http://jira.goupwith.com/browse/AGNES-221]

--- src/modules/agnes-tc/pages/config/kpi-def/index.vue
+++ src/modules/agnes-tc/pages/config/kpi-def/index.vue
@@ -101,6 +101,7 @@
           this.taskCommit.stepInfo.stepCode = this.row.stepCode;
           this.kpiDetail.kpiCode= this.row.taskKey;
           this.form.bizDate= this.row.bizDt;
+            this.form.createTime = this.row.taskStartTm
           this.data.q.bizDate = this.row.bizDt;
           this.init();
         },
@@ -154,7 +155,6 @@
                 this.executeKpi()
             },
             async onExtendButton(){//点击强制通过的事件
-                // if(this.row.allowManualConfirm && this.row.allowManualConfirm === '1'){
                     this.taskCommit.stepInfo.reason = this.form.reason;
                     this.taskCommit.stepInfo.stepStatus = "07";
                     try {
@@ -289,7 +289,11 @@
     }
 
     .item {
-        margin-bottom: 18px;
+        /*margin-bottom: 18px;*/
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
     }
 
     .clearfix:before,
@@ -307,9 +311,10 @@
         margin-top: 10px;
         margin-bottom: 30px;
         margin-left: 40px;
+        margin-right: 40px;
         float: left;
-        width: 33%;
-        max-width: 180px;
+        /*width: 33%;*/
+        /*max-width: 180px;*/
     }
 
     .first{
