b62f9caf76d655f1fd9df8bd574f7eb6e0bb9304
梁成 <cheng.liang@goupwith.com>
2020-08-25 17:29:44 +0800

    AGNES-58 待办任务界面 http://jira.goupwith.com/browse/AGNES-58

--- src/modules/agnes-tc/pages/config/index.js
+++ src/modules/agnes-tc/pages/config/index.js
@@ -1,4 +1,5 @@
 import KpiDef from "./kpi-def/index";
+import KpiManul from "./kpi-def/manul";
 import TaskTodo from "./task-todo/index";
 import TaskDone from "./task-done/index";
 import TaskManage from "./task-manage/index";
@@ -6,6 +7,7 @@ import TaskManage from "./task-manage/index";
 const Loading = {
     load: function (Gfui) {
         Gfui.componentView('agnes.kpi.def', KpiDef, {title: '指标任务详情'});
+        Gfui.componentView('agnes.kpi.manul', KpiManul, {title: '指标结果人工一致'});
         Gfui.componentView('agnes.app.task.todo', TaskTodo, {title: '待办任务'});
         Gfui.componentView('agnes.app.task.done', TaskDone, {title: '办结任务'});
         Gfui.componentView('agnes.app.task.mgr', TaskManage, {title: '任务管理'});
