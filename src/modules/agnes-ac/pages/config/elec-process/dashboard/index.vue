876be89969690652b3ec509b4de12b47f9a1cbd7
gy <gy.hu@goupwith.com>
2020-09-11 15:05:59 +0800

    AGNES-208 电子流程操作 http://jira.goupwith.com/browseAGNES-208 领导驾驶舱、电子操作流程、case组件preview模式

--- src/modules/agnes-ac/pages/config/elec-process/dashboard/index.vue
+++ src/modules/agnes-ac/pages/config/elec-process/dashboard/index.vue
@@ -2,33 +2,39 @@
     <el-container class="elec-process elec-process-dashboard">
         <section class="dataSearch">
             <span style="line-height: 28px">业务日期：</span>
-            <el-date-picker
-                    v-model="bizDate"
-                    type="date"
-                    size="mini"
-                    align="center"
-                    :clearable="false"
-                    value-format="yyyy-MM-dd"
-                    placeholder="选择日期">
+            <el-date-picker v-model="bizDate"
+                type="date"
+                size="mini"
+                align="center"
+                :clearable="false"
+                value-format="yyyy-MM-dd"
+                placeholder="选择日期"
+                style="width: 175px"
+                @change="bizDateChange"
+            >
             </el-date-picker>
-            <i class="el-icon-refresh" title="全部刷新"></i>
+            <i class="el-icon-refresh" title="全部刷新" @click="getFLowsbyType(bizDate)"></i>
         </section>
-        <section class="card-container" v-if="dashBoardData">
-            <module-card shadow="always" :class="currentBoard.boardId==board.boardId?'active':''"
-                         v-for="board in dashBoardData" :key="board.boardId" @click.native="chooseBoard(board)">
+        <section class="card-container" v-if="proTask">
+            <module-card shadow="always" :class="curTask.taskId==task.taskId?'active':''"
+                         v-for="task in proTask" :key="task.taskId" @click.native="chooseTask(task)">
                 <template slot="content">
-                    <p class="imgContainer">
-                        <i v-html="svgImg.process"></i>
+                    <p class="imgContainer" :style="{color: getStatusColor(task.taskStatus)}">
+                        <i v-if="task.taskIcon" :class="task.taskIcon"></i>
+                        <i v-else class="fa fa-cogs"></i>
                     </p>
-                    <p class="title">{{board.boardName}}</p>
-                    <p>
-                        <el-progress :percentage="parseInt(board.completionPer*100)" :stroke-width="8"
-                                     color="#476DBE"></el-progress>
+                    <p class="title">{{task.taskName}}</p>
+                    <p style="height: 15px">
+                        <el-progress v-if="task.finishedRate" class="monitor-progress" show-text
+                                     :percentage="getPercentage(task.finishedRate)"
+                                     :color="getStatusColor(task.taskStatus)"
+                                     :stroke-width="6"
+                        ></el-progress>
                     </p>
                 </template>
             </module-card>
-            <template v-if="dashBoardData.length%5 != 0">
-                <module-card v-for="restIndex in (5-dashBoardData.length%5)" :key="restIndex" style="opacity: 0">
+            <template v-if="proTask.length%5 != 0">
+                <module-card v-for="restIndex in (5-proTask.length%5)" :key="restIndex" style="opacity: 0">
                 </module-card>
             </template>
         </section>
@@ -36,33 +42,60 @@
 </template>
 
 <script>
-    import mockData from "../mockData";
     import boardDetail from './board-detail';
     export default {
         data(){
             return {
                 svgImg: this.$lcImg,
-                dashBoardData: mockData().processList || [],
+                proTask: [],
                 bizDate: '',
-                currentBoard: {},
-                parseInt: function (str) {
-                    if(str){
-                        return parseInt(str);
-                    }
-                }
+                curTask: {}
             }
         },
+        async created(){
+            // 默认系统业务日期
+            this.bizDate = window.bizDate;
+            this.getFLowsbyType(this.bizDate);
+        },
         methods: {
-            chooseBoard(board){
-                this.currentBoard = board;
+            // 业务日期切换
+            bizDateChange(val) {
+                this.getFLowsbyType(val);
+            },
+
+            // 根据流程类型加载对应流程数据
+            async getFLowsbyType(bizDate) {
+                const flowDataRes = this.$api.elecProcessApi.queryExecRuTask({bizDate});
+                const flowDataList = await this.$app.blockingApp(flowDataRes);
+                if (flowDataList.data && flowDataList.data.length > 0) {
+                    this.proTask = flowDataList.data;
+                }else{
+                    this.proTask = [];
+                }
+            },
+
+            // 选择流程
+            chooseTask(task){
+                this.curTask = task;
                 this.$drawerPage.create({
                     className: 'elec-dashboard-drawer',
                     width: 'calc(97% - 215px)',
-                    title: ['电子流程看板', 'view'],
+                    title: [`${task.taskName}【${this.bizDate}】`],
                     component: boardDetail,
-                    args: {}
+                    args: {bizDate: this.bizDate, task: task},
+                    cancelButtonTitle: '返回',
+                    okButtonTitle: '刷新'
                 });
-            }
+            },
+
+            getPercentage(percentage) {
+                return parseInt(percentage * 100) ;
+            },
+
+            getStatusColor(statusId) {
+                const stepStatus = this.$agnesAcUtils.getStepStatusMap();
+                return stepStatus.get(statusId).color;
+            },
         }
     }
 </script>
--- src/modules/agnes-ac/util/common.js
+++ src/modules/agnes-ac/util/common.js
@@ -21,13 +21,18 @@ function getStepStatusMap() {
             '04': '#F5222E',    // 已超时
             '05': '#52C41C',    // 已作废
             '06': '#52C41C',    // 已完成
-            '07': '#FAAE14',    // 人工强制关闭
+            '07': '#FAAE14',    // 强制关闭
+        }
+        const statusIcon = {
+            '02':'executing', '03':'outTime', '04':'abnormal',
+            '06':'finish'
         }
         stepStatus.forEach((statusItem)=>{
             this.stepStatus.set(statusItem.dictId, {
                 dictId: statusItem.dictId,
                 dictName:  statusItem.dictName,
-                color: colorSet[statusItem.dictId]
+                color: colorSet[statusItem.dictId],
+                icon: statusIcon[statusItem.dictId] || false
             });
         });
     }
