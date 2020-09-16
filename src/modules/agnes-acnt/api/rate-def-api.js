76084cf1039dcc6585246186ee0aee489a5cef54
shenjianqi <s815863039@163.com>
2020-09-16 17:14:21 +0800

    方案利率维护

--- src/modules/agnes-acnt/api/rate-def-api.js
+++ src/modules/agnes-acnt/api/rate-def-api.js
@@ -1,17 +1,14 @@
 import request from '@hex/gf-ui/src/util/request';
 
 export default {
-    queryRateList() {
-        return request.get(`/agnes-app/v1/acnt/ref/rate/list`);
-    },
-    save(form) {
-        return request.post(`/agnes-app/v1/acnt/ref/rate/save`,form);
-    },
-    changeStatus(form) {
-        return request.post(`/agnes-app/v1/acnt/rate/ref/change-status`,form);
-    },
-    deleteRate(pkId) {
-        return request.post("/agnes-app/v1/acnt/rate/ref/delete", {pkId:pkId});
+    saveRate(form) {
+        return request.post(`/agnes-app/v1/acnt/rate/info/save`,form);
     },
 
+    deleteRate(rateId) {
+        return request.post("/agnes-app/v1/acnt/rate/info/delete", null,{params: {rateId}});
+    },
+    getBranchList(form) {
+        return request.post(`/agnes-app/v1/dop/bank/get/list`,form);
+    },
 };
