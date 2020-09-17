8db68bf7b15f3ae04f677f4dd5346a4b786fe31a
shenjianqi <s815863039@163.com>
2020-09-17 17:40:37 +0800

    方案利率优化

--- src/modules/agnes-acnt/pages/config/acnt-rate/acnt-rate-detail.vue
+++ src/modules/agnes-acnt/pages/config/acnt-rate/acnt-rate-detail.vue
@@ -2,51 +2,48 @@
     <div>
         <el-form class="fit-box" :disabled="mode==='view'" :model="form" ref="form" :rules="rules" label-width="85px"
                  style="padding: 10px;">
-        <el-form-item label="方案代码" prop="rateCode">
-            <gf-input v-model="form.rateCode" placeholder="方案代码" :max-byte-len="8"/>
-        </el-form-item>
-        <el-form-item label="方案名称" prop="rateName">
-            <gf-input v-model="form.rateName" placeholder="方案名称" :max-byte-len="120"/>
-        </el-form-item>
+            <el-form-item label="方案代码" prop="rateCode">
+                <gf-input v-model="form.rateCode" placeholder="方案代码" :max-byte-len="8"/>
+            </el-form-item>
+            <el-form-item label="方案名称" prop="rateName">
+                <gf-input v-model="form.rateName" placeholder="方案名称" :max-byte-len="120"/>
+            </el-form-item>
             <el-form-item label="网点名称" prop="bankBranchId">
                 <el-select class="multiple-select" v-model="form.bankBranchId"
                            filterable clearable
                            placeholder="请选择">
                     <gf-filter-option
-                        v-for="item in branchList"
-                        :key="item.bankBranchId"
-                        :label="item.label"
-                        :value="item.bankBranchId">
+                            v-for="item in branchList"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
                     </gf-filter-option>
                 </el-select>
             </el-form-item>
-<!--        <el-form-item label="网点代码" prop="branchCode">
-            <gf-input v-model="form.branchCode" disabled placeholder="网点代码" :max-byte-len="120"/>
-        </el-form-item>-->
-        <el-form-item label="利率" prop="rate">
-            <gf-input v-model="form.rate" placeholder="利率" :max-byte-len="120"/>
-        </el-form-item>
-        <el-form-item label="生效日期" prop="startDt">
-            <div class="line none-shrink">
-                <el-form-item prop="startDt">
-                    <el-date-picker
-                            v-model="form.startDt"
-                            type="date"
-                            value-format="yyyy-MM-dd"
-                            placeholder="开始日期">
-                    </el-date-picker>
-                </el-form-item>
-                <span style="margin: 0 10px">~</span>
-                <el-form-item prop="endDt">
-                    <el-date-picker
-                            v-model="form.endDt"
-                            type="date"
-                            value-format="yyyy-MM-dd"
-                            placeholder="结束日期">
-                    </el-date-picker>
-                </el-form-item>
-            </div>
-        </el-form-item>
+            <el-form-item label="利率" prop="rate">
+                <gf-input v-model="form.rate" placeholder="利率" :max-byte-len="120"/>
+            </el-form-item>
+            <el-form-item label="生效日期" prop="startDt">
+                <div class="line none-shrink">
+                    <el-form-item prop="startDt">
+                        <el-date-picker
+                                v-model="form.startDt"
+                                type="date"
+                                value-format="yyyy-MM-dd"
+                                placeholder="开始日期">
+                        </el-date-picker>
+                    </el-form-item>
+                    <span style="margin: 0 10px">~</span>
+                    <el-form-item prop="endDt">
+                        <el-date-picker
+                                v-model="form.endDt"
+                                type="date"
+                                value-format="yyyy-MM-dd"
+                                placeholder="结束日期">
+                        </el-date-picker>
+                    </el-form-item>
+                </div>
+            </el-form-item>
         </el-form>
         <dialog-footer :ok-button="mode !== 'view'" :on-save="onSave" ok-button-title="确定"></dialog-footer>
     </div>
@@ -59,23 +56,23 @@
                 type: String,
                 default: 'add'
             },
-            ui:String,
+            ui: String,
             row: Object,
             actionOk: Function
         },
         data() {
             return {
-                form:{
-                    rateCode:'',
-                    rateName:'',
-                   branchCode:'',
-                    branchName:'',
-                    startDt:'',
-                    endDt:'',
-                    rate:'',
-                    status:'',
-                    bankBranchId:'',
-                    rateId:'',
+                form: {
+                    rateCode: '',
+                    rateName: '',
+                    branchCode: '',
+                    branchName: '',
+                    startDt: '',
+                    endDt: '',
+                    rate: '',
+                    status: '',
+                    bankBranchId: '',
+                    rateId: '',
                 },
                 branchList: [],
                 rules: {
@@ -92,25 +89,15 @@
             Object.assign(this.form, this.row);
             this.getOptionData()
         },
-      /*  watch: {
-            'form.bankBranchId' (val) {
-                if (val) {
-                   for (let i = 0; i <list.length; i++){
-                       if (this.form.bankBranchId === this.branchList[i].bankBranchId) {
-                           this.form.branchCode =this.branchList[i].branchCode;
-                       }
-                   }
-                }
-            },
-        },*/
+
         methods: {
-            async getOptionData(){
+            async getOptionData() {
                 try {
                     let groupOption = await this.$api.rateDefApi.getBranchList();
-                    let list = groupOption.data
-                    list.forEach((item)=>{
-                        let branchName = '('+item.branchCode+')'+item.branchName;
-                        this.branchList.push({label:branchName,value:item.branchCode});
+                    let list = groupOption.data;
+                    list.forEach((item) => {
+                        let branchName = '(' + item.branchCode + ')' + item.branchName;
+                        this.branchList.push({label: branchName, value: item.bankBranchId});
                     });
                 } catch (reason) {
                     this.$msg.error(reason);
@@ -122,12 +109,14 @@
                     return;
                 }
                 try {
-                    if(this.ui==="1"){
+                    if (this.ui === "1") {
                         let p = this.$api.rateDefApi.check(this.form);
                         await this.$app.blockingApp(p);
                         this.$msg.success('审核通过');
 
-                    }else{
+                    } else {
+                        // eslint-disable-next-line no-console
+                        console.log(this.form);
                         let p = this.$api.rateDefApi.saveRate(this.form);
                         await this.$app.blockingApp(p);
                         this.$msg.success('保存成功');
