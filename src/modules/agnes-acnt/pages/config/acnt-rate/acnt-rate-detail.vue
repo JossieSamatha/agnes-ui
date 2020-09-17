6590c61f9456a6a19aa6cff339d7c0c22892b9c0
shenjianqi <s815863039@163.com>
2020-09-17 16:37:06 +0800

    方案利率优化

--- src/modules/agnes-acnt/pages/config/acnt-rate/acnt-rate-detail.vue
+++ src/modules/agnes-acnt/pages/config/acnt-rate/acnt-rate-detail.vue
@@ -8,27 +8,23 @@
         <el-form-item label="方案名称" prop="rateName">
             <gf-input v-model="form.rateName" placeholder="方案名称" :max-byte-len="120"/>
         </el-form-item>
-            <el-form-item label="网点名称" prop="branchName">
+            <el-form-item label="网点名称" prop="bankBranchId">
                 <el-select class="multiple-select" v-model="form.bankBranchId"
                            filterable clearable
                            placeholder="请选择">
                     <gf-filter-option
                         v-for="item in branchList"
                         :key="item.bankBranchId"
-                        :label="item.branchName"
+                        :label="item.label"
                         :value="item.bankBranchId">
                     </gf-filter-option>
                 </el-select>
             </el-form-item>
-        <el-form-item label="网点代码" prop="branchCode">
-            <gf-input v-model="form.branchCode" placeholder="网点代码" :max-byte-len="120"/>
-        </el-form-item>
+<!--        <el-form-item label="网点代码" prop="branchCode">
+            <gf-input v-model="form.branchCode" disabled placeholder="网点代码" :max-byte-len="120"/>
+        </el-form-item>-->
         <el-form-item label="利率" prop="rate">
             <gf-input v-model="form.rate" placeholder="利率" :max-byte-len="120"/>
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
-            <gf-dict-select v-model.trim="form.status" dict-type="AGNES_RELEASE_STATUS" placeholder="状态" :max-byte-len="120"/>
-
         </el-form-item>
         <el-form-item label="生效日期" prop="startDt">
             <div class="line none-shrink">
@@ -63,6 +59,7 @@
                 type: String,
                 default: 'add'
             },
+            ui:String,
             row: Object,
             actionOk: Function
         },
@@ -71,7 +68,7 @@
                 form:{
                     rateCode:'',
                     rateName:'',
-                    branchCode:'',
+                   branchCode:'',
                     branchName:'',
                     startDt:'',
                     endDt:'',
@@ -84,7 +81,7 @@
                 rules: {
                     'rateCode': [{required: true, message: "方案代码不能为空"}],
                     'rateName': [{required: true, message: "方案代码不能为空"}],
-                    // 'branchName': [{required: true, message: "网点名称不能为空"}],
+                    'bankBranchId': [{required: true, message: "网点名称不能为空"}],
                     'startDate': [{required: true, message: "生效日期不能为空"}],
                     'rate': [{required: true, message: "方案利率不能为空"}],
                     'status': [{required: true, message: "状态不能为空"}],
@@ -95,22 +92,26 @@
             Object.assign(this.form, this.row);
             this.getOptionData()
         },
-        watch: {
+      /*  watch: {
             'form.bankBranchId' (val) {
                 if (val) {
-                   for (let i = 0; i <this.branchList.length; i++){
+                   for (let i = 0; i <list.length; i++){
                        if (this.form.bankBranchId === this.branchList[i].bankBranchId) {
                            this.form.branchCode =this.branchList[i].branchCode;
                        }
                    }
                 }
             },
-        },
+        },*/
         methods: {
             async getOptionData(){
                 try {
                     let groupOption = await this.$api.rateDefApi.getBranchList();
-                    this.branchList = groupOption.data
+                    let list = groupOption.data
+                    list.forEach((item)=>{
+                        let branchName = '('+item.branchCode+')'+item.branchName;
+                        this.branchList.push({label:branchName,value:item.branchCode});
+                    });
                 } catch (reason) {
                     this.$msg.error(reason);
                 }
@@ -121,12 +122,19 @@
                     return;
                 }
                 try {
-                    const p = this.$api.rateDefApi.saveRate(this.form);
-                    await this.$app.blockingApp(p);
+                    if(this.ui==="1"){
+                        let p = this.$api.rateDefApi.check(this.form);
+                        await this.$app.blockingApp(p);
+                        this.$msg.success('审核通过');
+
+                    }else{
+                        let p = this.$api.rateDefApi.saveRate(this.form);
+                        await this.$app.blockingApp(p);
+                        this.$msg.success('保存成功');
+                    }
                     if (this.actionOk) {
                         await this.actionOk(this.form, this.row);
                     }
-                    this.$msg.success('保存成功');
                     this.$dialog.close(this);
                 } catch (e) {
                     this.$msg.error(e);
