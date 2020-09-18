f0a7014cdbb285172fd6cb274f3bd2cd978ab619
Fengss <shisheng.feng@goupwith.com>
2020-09-18 14:13:54 +0800

    AGNES-297 账户类型配置 http://jira.goupwith.com/browse/AGNES-297

--- src/modules/agnes-acnt/api/acnt-info-api.js
+++ src/modules/agnes-acnt/api/acnt-info-api.js
@@ -4,6 +4,9 @@ import request from '@hex/gf-ui/src/util/request';
 export default {
     getAcntInfoList() {
         return request.get(`/agnes-app/v1/acnt/info/list`);
-    }
+    },
 
+    getAcntInfoByAcntId(acntId) {
+        return request.post("/agnes-app/v1/acnt/info/id", null, {params: {acntId}});
+    }
 };
