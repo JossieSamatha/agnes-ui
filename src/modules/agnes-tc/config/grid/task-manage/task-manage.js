36df8b8dab328cdf159da08ed0d5f8fa75abad2d
LH <17835418239@163.com>
2020-09-22 14:07:19 +0800

    AGNES-281 任务配置页面补充 [http://jira.goupwith.com/browse/AGNES-281]

--- src/modules/agnes-tc/config/grid/task-manage/task-manage.js
+++ src/modules/agnes-tc/config/grid/task-manage/task-manage.js
@@ -3,13 +3,13 @@ import column from "../../../../../config/column";
 const colButtons = [
     {key: 'checkTask', title: '复核',disabled: (params)=>{
             let result = true;
-            if(params.taskStatus == '01'){
-                result =false;
+            if(params.data.taskStatus == '00'){
+                result = false;
             }
             return result;}},
     {key: 'cancelTask', title: '作废', cellClass: 'red-cell',disabled: (params)=>{
             let result = true;
-            if(params.taskStatus != '05'){
+            if(params.data.taskStatus != '05'){
                 result =false;
             }
             return result;}},
