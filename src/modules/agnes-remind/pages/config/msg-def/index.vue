c5bcf8de1e2c80a863cfe89dbec46c929d5d2083
fm.li <fm.li@goupwith.com>
2020-09-30 16:49:28 +0800

    待办/消息样式修改

--- src/modules/agnes-remind/pages/config/msg-def/index.vue
+++ src/modules/agnes-remind/pages/config/msg-def/index.vue
@@ -1,16 +1,106 @@
 <template>
     <div>
-        <gf-grid @row-double-click="showMsg" grid-no="agnes-msg-type" ref="grid" toolbar="find,refresh,more">
-            <template slot="left">
-                <gf-button class="action-btn" @click="batchRead" size="mini">批量读取</gf-button>
-            </template>
-        </gf-grid>
+<!--        <gf-grid @row-double-click="showMsg" grid-no="agnes-msg-type" ref="grid" toolbar="find,refresh,more">-->
+<!--            <template slot="left">-->
+<!--                <gf-button class="action-btn" @click="batchRead" size="mini">批量读取</gf-button>-->
+<!--            </template>-->
+<!--        </gf-grid>-->
+        <gf-button class="action-btn" @click="batchRead" size="mini">批量读取</gf-button>
+        <gf-button class="action-btn" size="mini">全选</gf-button>
+        <div class="task-container" v-for="(item, index) in msgDemoArr" :key="index">
+            <el-checkbox v-model="checkbox" checked class="task-type">
+                {{item.msgType}}
+            </el-checkbox>
+            <span class="msg-time">{{item.msgTime}}</span>
+            <div class="task-content">
+                {{item.msgContent}}
+             </div>
+            <div :class="setClassName(item.msgState)">
+                {{item.msgState}}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import loadsh from'lodash';
     export default {
+        data () {
+            return {
+                msgDemoArr: [
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "完成"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "超时"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "提醒"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "完成"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "超时"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "提醒"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "完成"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "超时"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "提醒"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "完成"
+                    },
+                ]
+            }
+        },
         methods: {
              async batchRead() {
                 let data = this.$refs.grid.getSelectedRows();
@@ -34,10 +124,84 @@
             reloadData() {
                 this.$refs.grid.reloadData();
             },
+            setClassName(val) {
+                if (val === "完成") {
+                    return "task-state-done"
+                }
+                if (val === "提醒") {
+                    return "task-state-clock"
+                }
+                if (val === "超时") {
+                    return "task-state-overtime"
+                }
+            }
         },
     }
 </script>
 
 <style scoped>
-
+    .task-container {
+        width: 95%;
+        height: 106px;
+        background: #FFFFFF;
+        border: 1px solid #E5E7E9;
+        border-radius: 4px;
+        margin-left: 20px;
+        margin-bottom: 30px;
+        position: relative;
+        overflow: hidden;
+    }
+    .task-type {
+        margin-left: 40px;
+        margin-top: 15px;
+        color: #333;
+        font-size: 12px;
+    }
+    .task-content {
+        margin-left: 40px;
+        margin-top: 15px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #656565;
+        font-size: 12px;
+    }
+    .task-state-done,
+    .task-state-overtime,
+    .task-state-clock {
+        position: absolute;
+        height: 18px;
+        line-height: 18px;
+        text-align: center;
+        width: 70px;
+        color: #fff;
+        transform: rotate(45deg);
+        right: -18px;
+        top: 9px;
+        font-size: 12px;
+    }
+    .task-state-done {
+        background-color: #74c640;
+    }
+    .task-state-overtime {
+        background-color: #e36a59;
+    }
+    .task-state-clock {
+        background-color: #ffa920;
+    }
+    .msg-time {
+        color: #999;
+        font-size: 12px;
+        position: absolute;
+        right: 50px;
+        top: 20px;
+    }
+    .gf-tab-view {
+        display: block !important;
+        overflow: scroll;
+    }
+    .el-button--mini, .el-button--mini.is-round {
+        margin-left: 20px;
+        margin-bottom: 15px;
+    }
 </style>
--- src/modules/agnes-tc/pages/config/task-todo/index.vue
+++ src/modules/agnes-tc/pages/config/task-todo/index.vue
@@ -1,25 +1,49 @@
 <template>
+<!--任务中心-->
     <div>
-            <el-form class="search-panel" label-width="75px">
-                <div class="line">
-                    <el-form-item label="任务名称">
-                        <el-input v-model="queryArgs.taskName"></el-input>
-                    </el-form-item>
-                    <el-form-item label="发起时间">
-                        <el-date-picker
-                                v-model="queryArgs.taskStartTime"
-                                type="date"
-                                value-format="yyyy-MM-dd"
-                                placeholder="">
-                        </el-date-picker>
-                    </el-form-item>
-                    <el-button @click="reloadData" class="option-btn" type="primary">查询</el-button>
-                    <el-button @click="reSetSearch" class="option-btn">重置</el-button>
-                </div>
-            </el-form>
-            <gf-grid :query-args="queryArgs" @row-double-click="viewTask" grid-no="agnes-task-todo" ref="grid">
-
-            </gf-grid>
+        <el-form class="search-panel" label-width="75px">
+            <div class="line">
+                <el-form-item label="任务名称">
+                    <el-input v-model="queryArgs.taskName"></el-input>
+                </el-form-item>
+                <el-form-item label="发起时间">
+                    <el-date-picker
+                      v-model="queryArgs.taskStartTime"
+                      type="date"
+                      value-format="yyyy-MM-dd"
+                      placeholder="">
+                    </el-date-picker>
+                </el-form-item>
+                <el-button @click="reloadData" class="option-btn" type="primary">查询</el-button>
+                <el-button @click="reSetSearch" class="option-btn">重置</el-button>
+            </div>
+        </el-form>
+<!--        <gf-grid -->
+<!--            :query-args="queryArgs" -->
+<!--            @row-double-click="viewTask" -->
+<!--            grid-no="agnes-task-todo" -->
+<!--            ref="grid"-->
+<!--        >-->
+<!--        </gf-grid>-->
+        <div class="task-container" v-for="(item, index) in msgDemoArr" :key="index">
+            <div class="task-type">{{item.msgType}}</div>
+            <div class="task-content">
+                {{item.msgContent}}
+            </div>
+            <div class="task-execor">
+                <span>{{item.msgSender}}</span>
+                <span style="margin-left: 20px;">{{item.msgTime}}</span>
+                <span style="position: absolute;
+                             right: 30px;
+                             color: #476DBD;"
+                >
+                    去处理
+                </span>
+            </div>
+            <div :class="setClassName(item.msgState)">
+                {{item.msgState}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -34,7 +58,79 @@
                 queryArgs:{
                     'taskName':'',
                     'taskStartTime':''
-                }
+                },
+                msgDemoArr: [
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "已完成"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "未开始"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "执行中"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "有异常"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "已超时"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "未开始"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "执行中"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "已超时"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "有异常"
+                    },
+                    {
+                        msgType: "【数据收发】启动自动发送-处理",
+                        msgContent: "这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容，这里是消息内容",
+                        msgSender: "通知人员: 张三",
+                        msgTime: "2020-09-28 15:30:00",
+                        msgState: "已完成"
+                    },
+                ]
             }
         },
         mounted() {
@@ -97,10 +193,10 @@
                         });
                     }
                 },
-                async onViewTask() {
+            async onViewTask() {
                     this.reloadData();
                 },
-                confirmTask(params) {
+            confirmTask(params) {
                     const row = params.data;
                     let mode = 'edit';
                     let actionOk = this.onConfirmTask.bind(this);
@@ -115,9 +211,96 @@
                         );
                     }
                 },
-                async onConfirmTask() {
+            async onConfirmTask() {
                     this.reloadData();
                 },
+            setClassName(val) {
+                if (val === "已完成") {
+                    return "task-state-done"
+                }
+                if (val === "未开始") {
+                    return "task-state-start"
+                }
+                if (val === "已超时") {
+                    return "task-state-overtime"
+                }
+                if (val === "执行中") {
+                    return "task-state-exec"
+                }
+                if (val === "有异常") {
+                    return "task-state-error"
+                }
             }
         }
+    }
 </script>
+<style scoped>
+    .task-container {
+        width: 95%;
+        height: 123px;
+        background: #FFFFFF;
+        border: 1px solid #E5E7E9;
+        border-radius: 4px;
+        margin-left: 20px;
+        margin-bottom: 30px;
+        position: relative;
+        overflow: hidden;
+    }
+    .task-type {
+        margin-left: 40px;
+        margin-top: 15px;
+        color: #333;
+        font-size: 12px;
+    }
+    .task-content {
+        margin-left: 40px;
+        margin-top: 15px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #656565;
+        font-size: 12px;
+    }
+    .task-execor {
+        height: 38px;
+        border-top: 1px solid #cccccc;
+        line-height: 38px;
+        padding-left: 40px;
+        margin-top: 15px;
+        color: #999999;
+        font-size: 12px;
+        position: relative;
+    }
+    .task-state-done,
+    .task-state-overtime,
+    .task-state-start,
+    .task-state-exec,
+    .task-state-error {
+        position: absolute;
+        height: 18px;
+        line-height: 18px;
+        text-align: center;
+        width: 70px;
+        color: #fff;
+        transform: rotate(45deg);
+        right: -18px;
+        top: 9px;
+        font-size: 12px;
+    }
+    .task-state-done,
+    .task-state-start,
+    .task-state-exec {
+        background-color: #6895f2;
+    }
+    .task-state-error,
+    .task-state-overtime {
+        background-color: #ea6461;
+    }
+    .gf-tab-view {
+        display: block !important;
+        overflow: scroll;
+    }
+    .el-form.search-panel {
+        margin-bottom: 20px;
+    }
+</style>
