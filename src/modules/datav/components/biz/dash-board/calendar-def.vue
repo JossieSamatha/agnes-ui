111660b92e9595a948d878055197410b0b046013
gy <gy.hu@goupwith.com>
2020-10-15 16:22:42 +0800

    AGNES-208 电子流程操作 http://jira.goupwith.com/browseAGNES-208 综合监控看板 -- 样式相关调整

--- src/modules/datav/components/biz/dash-board/calendar-def.vue
+++ src/modules/datav/components/biz/dash-board/calendar-def.vue
@@ -1,6 +1,13 @@
 <template>
-    <el-calendar v-model="moduleObj.calendarVal" :first-day-of-week="7">
-    </el-calendar>
+    <agnes-calendar ref="calendar" v-model="moduleObj.calendarVal" :first-day-of-week="7">
+        <template slot="dateCell" slot-scope="{date}">
+            <el-popover placement="bottom" width="110" trigger="click" popper-class="calendar-popper">
+                <p>运营日历：<span>5个</span></p>
+                <el-button type="text">查看详情</el-button>
+                <el-button slot="reference">{{ getDay(date) }}</el-button>
+            </el-popover>
+        </template>
+    </agnes-calendar>
 </template>
 
 <script>
@@ -11,8 +18,19 @@
                 required: true
             }
         },
-        mounted() {
-            this.moduleObj
+        created(){
+            this.$dataVBus.$off('clientCalendarRefresh');
+            this.$dataVBus.$on('clientCalendarRefresh', this.clientCalendarRefresh);
+            this.$api.workdayConfigApi.getWorkdayList({'workdayAreaCode':'CN'});
+        },
+        methods: {
+            getDay(date){
+                return new Date(date).getDate()
+            },
+
+            clientCalendarRefresh(){
+                this.$refs.calendar.selectDate('today')
+            }
         }
     }
 </script>
