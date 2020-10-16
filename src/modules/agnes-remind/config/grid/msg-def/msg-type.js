b30ba34d79a1e23cdaefb512d6841aa897d4d85b
matian <tian.ma@goupwith.com>
2020-10-16 13:59:41 +0800

    AGNES-424 消息盒子数据 [http://jira.goupwith.com/browse/AGNES-424]

--- src/modules/agnes-remind/config/grid/msg-def/msg-type.js
+++ src/modules/agnes-remind/config/grid/msg-def/msg-type.js
@@ -15,7 +15,7 @@ export default {
         fetchUrl: "/agnes-remind/v1/remind/msg/query",    //后台查询数据的URL地址
         fetchMethod: 'get',
         pagingMode: true, //不分页
-        checkboxColumn: 1, //是否显示checkbox列,
+        checkboxColumn: 2, //是否显示checkbox列,
         enableExportLocal: true,
         autoFitColumnMode: 1,
         pageOptions: {
