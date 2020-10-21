c94df0bd67a7c3aaee096af42d3533f753536b94
LH <17835418239@163.com>
2020-10-21 09:52:47 +0800

    AGNES-281 通配符修改-选人组件接口修改 [http://jira.goupwith.com/browse/AGNES-281]

--- src/modules/agnes-dc/pages/config/file-scan-config/file-scan-config-detail.vue
+++ src/modules/agnes-dc/pages/config/file-scan-config/file-scan-config-detail.vue
@@ -39,7 +39,7 @@
                            icon="fa fa-question-circle-o"
                            style="border: none;padding: 0;font-size: 20px;vertical-align: middle"
                 ></el-button>
-                <p>格式：${[yyyymmdd/yyyy-mm-dd],-1/0/1,D/W,CN};</p>
+                <p>格式：${yyyymmdd/yyyy-mm-dd,-1/0/1,D/W,CN};</p>
                 <p>-1/0/1：前一天/当天/后一天;D:自然日、W:工作(注：选择当天，该参数应为D);CN:中国大陆</p>
                 <p>例如：/home/${[yyyymmdd],-2,D,CN}/test</p>
                 <p>如当天日期为20200925,该表达式解析为：/home/20200923/test</p>
@@ -56,7 +56,7 @@
                            icon="fa fa-question-circle-o"
                            style="border: none;padding: 0;font-size: 20px;vertical-align: middle"
                 ></el-button>
-                <p>格式：${[yyyymmdd/yyyy-mm-dd],-1/0/1,D/W,CN};</p>
+                <p>格式：${yyyymmdd/yyyy-mm-dd,-1/0/1,D/W,CN};</p>
                 <p>-1/0/1：前一天/当天/后一天;D:自然日、W:工作(注：选择当天，该参数应为D);CN:中国大陆</p>
                 <p>例如：/home/${[yyyymmdd],-2,D,CN}/test</p>
                 <p>如当天日期为20200925,该表达式解析为：/home/20200923/test</p>
