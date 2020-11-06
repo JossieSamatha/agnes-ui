1aca7893d29267b0f784a45f489dfda269579447
matian <tian.ma@goupwith.com>
2020-11-06 20:04:30 +0800

    AGNES-436 监控指标-人工改为“干预通过” http://jira.goupwith.com/browse/AGNES-436

--- src/modules/agnes-ac/pages/config/elec-process/optional/index.vue
+++ src/modules/agnes-ac/pages/config/elec-process/optional/index.vue
@@ -430,7 +430,7 @@
                     width: 'calc(97% - 215px)',
                     title: [row.stepName],
                     component: 'monitor-detail-page',
-                    args: {stepCode: row.kpiCode,stepActKey:row.stepActKey, bizDate: this.bizDate, status: 3},
+                    args: {stepCode: row.stepCode,stepActKey:row.stepActKey, bizDate: this.bizDate, status: 3},
                     cancelButtonTitle: '返回',
                     okButtonVisible: false
                 });
