4adbb04637667dae39fc0b360b6b260ca1aaab52
gy <gy.hu@goupwith.com>
2020-12-02 19:02:19 +0800

    AGNES-208 电子流程操作 http://jira.goupwith.com/browseAGNES-208 账户管理样式调整

--- src/modules/agnes-acnt/pages/config/acnt-apply/acnt-apply-steps.vue
+++ src/modules/agnes-acnt/pages/config/acnt-apply/acnt-apply-steps.vue
@@ -1,27 +1,26 @@
 <template>
     <div>
         <p class="pro-title">操作流程</p>
-        <el-steps class="step-vertical" direction="vertical" :active="activeStep" :space="50">
+        <el-steps class="step-vertical" direction="vertical" :active="activeStep" :space="65">
             <template v-for="(step, stepIndex) in stepArr">
                 <el-step :key="stepIndex"
-                        :title="step.stepTitle"
                          @click.native="activeStep===stepIndex ? stepClick(step) : false"
                         v-if="step.stepId !== '09'"
                 >
                     <template slot="icon">
                         <span v-html="lcImg[step.stepIcon]"></span>
                     </template>
+                    <template slot="title">
+                        <span>{{step.stepTitle}}</span>
+                        <span v-show="activeStep===stepIndex && canObsolete"
+                              class="obsolete-step"
+                              @click="stepDelete"
+                        >作废</span>
+                    </template>
                 </el-step>
             </template>
         </el-steps>
-        <div class="obsolete-step"
-             :class="canObsolete?'':'disabled'"
-        >
-            <span class="icon" v-html="lcImg.proObsolete"></span>
-            <span class="title" @click="canObsolete ? stepDelete() : false">作废申请</span>
-        </div>
     </div>
-
 </template>
 
 <script>
@@ -105,6 +104,8 @@
                     this.$emit('stepEdit', {data: this.stepData});
                 }else if(status === '02' || status === '04'|| status === '05'|| status === '07'){
                     this.$emit('stepCheck', {data: this.stepData});
+                }else if(status === '03'){
+                    this.$emit('submitOA');
                 }
             },
 
@@ -117,11 +118,10 @@
 
 <style>
     .pro-title {
-        line-height: 32px;
-        border-bottom: 1px solid #ccc;
-        text-align: center;
+        line-height: 35px;
         background: #eaedf1;
-        margin: 0 -10px 10px;
+        margin: 0 -20px 10px;
+        padding-left: 25px;
     }
 
     .step-vertical.el-steps.el-steps--vertical {
@@ -139,12 +139,12 @@
         text-align: center;
     }
 
-    .step-vertical .el-step__head .el-step__icon>span>svg {
-        color: #666;
+    .step-vertical .el-step__head.is-process .el-step__icon>span>svg {
+        fill: #0F5EFF;
     }
 
     .step-vertical .el-step__head.is-finish .el-step__icon>span>svg {
-        fill: #476DBE;
+        fill: #1CAD26;
     }
 
     .step-vertical .el-step__head.is-wait .el-step__icon>span>svg {
@@ -152,42 +152,64 @@
     }
 
     .step-vertical .el-step__head.is-wait .el-step__icon {
-        color: #ccc;
+        color: #999;
     }
 
-    .step-vertical .el-step__head.is-finish .el-step__line,
+    .step-vertical .el-step.is-vertical .el-step__line {
+        width: 0;
+        border: 1px dashed #999;
+        background: transparent;
+    }
+
+    .step-vertical .el-step.is-vertical .el-step__line .el-step__line-inner {
+        border-width: 0!important;
+    }
+
+    .step-vertical .el-step__head.is-finish .el-step__line {
+        border-color: #1CAD26;
+        border-style: solid;
+    }
+
     .step-vertical .el-step__head.is-process .el-step__line {
-        border-color: #476DBE;
-        background: #476DBE;
+        border-color: #0F5EFF;
     }
 
-    .step-vertical .el-step__main .el-step__title {
-        color: #666;
+    .step-vertical .el-step__head.is-wait .el-step__line {
+        border-color: #999;
+    }
+
+    .el-step.is-vertical .el-step__main .el-step__title {
         font-size: 14px;
         font-weight: normal;
+        display: inline-block;
+        padding: 0 8px;
+        border-radius: 5px;
     }
 
     .step-vertical .el-step__main .el-step__title.is-process {
+        position: relative;
+        color: #0F5EFF;
+        border: 1px solid #0f5eff;
         cursor: pointer;
     }
 
     .step-vertical .el-step__main .el-step__title.is-finish {
-        color: #476Dbe;
+        color: #1CAD26;
     }
 
     .step-vertical .el-step__main .el-step__title.is-wait {
-        color: #ccc;
+        color: #999;
     }
 
     .obsolete-step {
-        display: flex;
-        width: 100%;
-        height: 50px;
+        position: absolute;
         color: #f5222e;
+        top: -1px;
+        right: -60px;
     }
 
     .obsolete-step.disabled {
-        color: #ccc;
+        color: #999;
     }
 
     .obsolete-step .icon {
@@ -203,7 +225,7 @@
     }
 
     .obsolete-step.disabled .icon>svg {
-        fill: #ccc;
+        fill: #999;
     }
 
     .obsolete-step .title {
