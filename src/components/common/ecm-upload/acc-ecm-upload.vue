c0d7335ee56073f8245edb124d170c7da36add39
Fengss <shisheng.feng@goupwith.com>
2020-12-24 19:16:05 +0800

    AGNES-330 账户问题修改 http://jira.goupwith.com/browse/AGNES-330 1.文件上传支持bmp（注：另外还要修改ecm服务的配置文件）

--- src/components/common/ecm-upload/acc-ecm-upload.vue
+++ src/components/common/ecm-upload/acc-ecm-upload.vue
@@ -14,7 +14,7 @@
                :disabled="disabled"
                :show-file-list="false"
                :file-list="fileList"
-               accept=".doc,.docx,.ppt,.pptx,.xls,.xlsx,.txt,.pdf,.jpg,.jpeg,.png,.gif,.tif,.tiff,.zip,.rar"
+               accept=".doc,.docx,.ppt,.pptx,.xls,.xlsx,.txt,.pdf,.jpg,.jpeg,.png,.gif,.tif,.tiff,.bmp,.zip,.rar"
                v-loading.fullscreen.lock="uploadFileLoading"
                element-loading-background="rgba(0, 0, 0, 0.3)" element-loading-text="文件上传中，请稍后">
         <div>
@@ -157,13 +157,13 @@
             //上传之前
             checkFile(file) {
                 this.uploadFileLoading = true;
-                //doc,docx,ppt,pptx,xls,xlsx,txt,pdf,jpg,jpeg,png,gif,tif,tiff,zip,rar
+                //doc,docx,ppt,pptx,xls,xlsx,txt,pdf,jpg,jpeg,png,gif,tif,tiff,bmp,zip,rar
                 var fileType=file.name.substring(file.name.lastIndexOf('.')+1);
-                const isImage = fileType === 'jpg'||fileType ==='jpeg'||fileType ==='png'||fileType ==='gif'||fileType ==='tif'||fileType ==='tiff';
+                const isImage = fileType === 'jpg'||fileType ==='jpeg'||fileType ==='png'||fileType ==='gif'||fileType ==='tif'||fileType ==='tiff'||fileType ==='bmp';
                 const isOffice = fileType === 'doc'||fileType ==='docx'||fileType ==='ppt'||fileType ==='pptx'||fileType ==='xls'||fileType ==='xlsx';
                 const isOther = fileType === 'zip'||fileType ==='rar'||fileType ==='pdf'||fileType ==='txt';
                 if (!isImage && !isOffice && !isOther) {
-                    this.$msg.error('上传文件必须为如下格式：doc,docx,ppt,pptx,xls,xlsx,txt,pdf,jpg,jpeg,png,gif,tif,tiff,zip,rar');
+                    this.$msg.error('上传文件必须为如下格式：doc,docx,ppt,pptx,xls,xlsx,txt,pdf,jpg,jpeg,png,gif,tif,tiff,bmp,zip,rar');
                     this.uploadFileLoading = false;
                 }
 
