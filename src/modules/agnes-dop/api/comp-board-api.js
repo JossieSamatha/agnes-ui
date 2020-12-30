715370c197562e539045e0f5aedc4b5880d913db
LH <17835418239@163.com>
2020-12-30 10:13:04 +0800

    AGNES-281 面板补充 [http://jira.goupwith.com/browse/AGNES-281]

--- src/modules/agnes-dop/api/comp-board-api.js
+++ src/modules/agnes-dop/api/comp-board-api.js
@@ -5,8 +5,27 @@ export default {
     getDashboards(form){
         return request.post(`/agnes-app//v1/dop/board/query-board`,form);
     },
+    /**
+     * 编辑、保存
+     * @param form
+     * @returns {Promise<AxiosResponse<any>>}
+     */
     saveDashboards(form) {
         return request.post(`/agnes-app//v1/dop/board/save-board`, form);
+    },
+    /**
+     * 管理面板
+     * @return
+     */
+    manageDashboards(form) {
+        return request.post(`/agnes-app//v1/dop/board/manage-board`, form);
+    },
+    /**
+     * 切换面板，更换当前选中面板
+     * @return
+     */
+    signLastDashboard(form) {
+        return request.post(`/agnes-app//v1/dop/board/sign-board`, form);
     },
     getCompList() {
         return request.post("/agnes-app//v1/dop/board/query-comp");
