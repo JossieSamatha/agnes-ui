748cd5a1904b6c9ae614fe2cff01f963c0edf245
zi.wang <zi.wang@goupwith.com>
2021-01-20 18:26:31 +0800

    AGNES-477 运营日历接口改造 [http://jira.goupwith.com/browse/AGNES-477]

---
+++ src/modules/agnes-dop/pages/config/roster/roster-def.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <gf-grid ref="grid"
+             grid-no="agnes-dop-roster-def-list"
+             toolbar="find,refresh,more"
+             @row-double-click="showRosterDef"
+    >
+      <template slot="left">
+        <gf-button class="action-btn" @click="addRosterDef" size="mini">
+          添加
+        </gf-button>
+      </template>
+    </gf-grid>
+  </div>
+</template>
+
+<script>
+import rosterDefDlg from '../memo/roster-type-dlg';
+
+export default {
+  methods: {
+    reloadData() {
+      this.$refs.grid.reloadData(true);
+    },
+    async onAddRosterDef() {
+      this.reloadData();
+    },
+    async onEditRosterDef() {
+      this.reloadData();
+    },
+    editRosterDef(param) {
+      this.showScheduleDlg('add', param.date, this.onEditRosterDef.bind(this));
+    },
+    addRosterDef() {
+      this.showScheduleDlg('add', {}, this.onAddRosterDef.bind(this));
+    },
+    showScheduleDlg(mode, row, actionOk) {
+      this.$nav.showDialog(
+          rosterDefDlg,
+          {
+            args: {row, mode, actionOk},
+            width: '650px',
+            closeOnClickModal: false,
+            title: this.$dialog.formatTitle('智能排班', mode),
+          }
+      );
+    },
+    async deleteRosterDef(param) {
+      const ok = await this.$msg.ask(`确认删除所选排班数据吗, 是否继续?`);
+      if (!ok) {
+        return
+      }
+      try {
+        const p = this.$api.rosterApi.deleteRef(param.date.pkId);
+        await this.$app.blockingApp(p);
+        this.reloadData();
+      } catch (reason) {
+        this.$msg.error(reason);
+      }
+    },
+    async approveRosterDef(param) {
+      const ok = await this.$msg.ask(`确认复核所选排班数据吗, 是否继续?`);
+      if (!ok) {
+        return
+      }
+      try {
+        const p = this.$api.rosterApi.approve(param.date.pkId);
+        await this.$app.blockingApp(p);
+        this.reloadData();
+      } catch (reason) {
+        this.$msg.error(reason);
+      }
+    }
+  }
+}
+</script>
