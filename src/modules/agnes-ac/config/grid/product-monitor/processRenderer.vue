e3aee22dd752a9e13e151dcc79b60fdd6986dff9
gy <gy.hu@goupwith.com>
2021-03-16 17:57:13 +0800

    AGNES-208 电子操作流程 http://jira.goupwith.com/browseAGNES-208 产品监控详情页面

---
+++ src/modules/agnes-ac/config/grid/product-monitor/processRenderer.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-progress v-if="params.data"
+                 :percentage="percentage"
+                 show-text
+                 :color="getStatusColor"
+                 :style="{color: params.data.status === '01' ? '#4ACE69' : '#EE924C'}"
+                 style="width: 120px" :title="`${percentage}%`">
+    </el-progress>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                percentage: 0,
+                status: '',
+                statusColor: ''
+            }
+        },
+        beforeMount(){
+            if(this.params.data){
+                this.percentage = parseInt(this.params.value*100);
+            }
+        },
+        methods: {
+            getStatusColor(){
+                if(this.percentage > 100){
+                    return '#F5222E';
+                }else{
+                    return this.params.data.status === '01' ? '#4ACE69' : '#EE924C';
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .el-progress >>> .el-progress__text {
+        font-size: 12px!important;
+        color: inherit;
+    }
+</style>
