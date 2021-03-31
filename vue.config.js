18e42a059430f279fc87372b3ed851f974429524
yg.sun <yg.sun@goupwith.com>
2021-03-31 18:22:26 +0800

    AGNES-528 华安基金2.0.1升级UI http://jira.goupwith.com/browse/AGNES-528

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,38 @@ module.exports = {
     devServer: {
         port: 8006,
         proxy: {
+            '/api/ecm-server': {
+                target: 'http://127.0.0.1:8080/api/ecm-server',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/ecm-server': '/'
+                }
+            },
+            '/api/agnes-ec': {
+                target: 'http://127.0.0.1:9001',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/agnes-ec': '/'
+                }
+            },
+            '/api/agnes-remind': {
+                target: 'http://127.0.0.1:9004',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/agnes-remind': '/'
+                }
+            },
+            '/api/agnes-ac': {
+                target: 'http://127.0.0.1:9002',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/agnes-ac': '/'
+                }
+            },
             // '/api/ecm-server': {
             //     target: 'http://127.0.0.1:8080/api/ecm-server',
             //     ws: true,
@@ -37,14 +69,14 @@ module.exports = {
             //         '^/api/agnes-ac': '/'
             //     }
             // },
-            // '/api/agnes-app': {
-            //     target: 'http://127.0.0.1:9003',
-            //     ws: true,
-            //     changeOrigin: true,
-            //     pathRewrite: {
-            //         '^/api/agnes-app': '/'
-            //     }
-            // },
+            '/api/agnes-app': {
+                target: 'http://127.0.0.1:9003',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/agnes-app': '/'
+                }
+            },
             // '/api/agnes-datav': {
             //     target: 'http://172.18.21.191:7010',
             //     ws: true,
@@ -53,30 +85,30 @@ module.exports = {
             //         '^/api/agnes-datav': '/'
             //     }
             // },
-            // '/api/data-pipe': {
-            //     target: 'http://127.0.0.1:9021/api/data-pipe',
-            //     ws: true,
-            //     changeOrigin: true,
-            //     pathRewrite: {
-            //         '^/api/data-pipe': '/'
-            //     }
-            // },
-            // '/api/dop-bpmn': {
-            //     target: 'http://127.0.0.1:9023/api/dop-bpmn',
-            //     ws: true,
-            //     changeOrigin: true,
-            //     pathRewrite: {
-            //         '^/api/dop-bpmn': '/'
-            //     }
-            // },
-            // '/dop-kpi/': {
-            //     target: 'http://172.20.10.3:7002',
-            //     ws: true,
-            //     changeOrigin: true,
-            // },
+            '/api/data-pipe': {
+                target: 'http://127.0.0.1:9021/api/data-pipe',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/data-pipe': '/'
+                }
+            },
+            '/api/dop-bpmn': {
+                target: 'http://127.0.0.1:9023/api/dop-bpmn',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/dop-bpmn': '/'
+                }
+            },
+            '/dop-kpi/': {
+                target: 'http://172.20.10.3:7002',
+                ws: true,
+                changeOrigin: true,
+            },
             '/api/': {
                 // target: 'http://172.18.16.195:9000',
-                target: 'http://agnes.test.hexinfo.cn:8200/api',
+                target: 'http://agnes.dev.hexinfo.cn:8200/api',
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
@@ -92,3 +124,4 @@ module.exports = {
         }
     }
 };
+
